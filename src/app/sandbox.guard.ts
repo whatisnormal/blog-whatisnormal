ccdc945384a8d47d71e5f4b59438f53b1300dd64
whatisnormal <aco.rostro-0z@icloud.com>
2023-01-13 21:18:06 +0000

    Added rxjs calls

--- src/app/sandbox.guard.ts
+++ src/app/sandbox.guard.ts
@@ -11,7 +11,7 @@ export class SandboxGuard implements CanActivate {
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
 
-    return !!route.queryParamMap.get('tests');
+    return !!route.queryParamMap.get('option');
   }
 
 }
--- src/app/sandbox/rxjs/rxjs.component.ts
+++ src/app/sandbox/rxjs/rxjs.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { combineLatest, forkJoin, interval, Subject, switchMap, take } from 'rxjs';
+import { combineLatest, concat, forkJoin, interval, map, merge, mergeAll, mergeMap, of, scan, Subject, switchMap, take, toArray } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { MockDataService } from './mock-data.service';
 
 @Component({
   selector: 'app-rxjs',
@@ -14,65 +15,98 @@ export class RxjsComponent implements OnInit{
   private entityEditedSubject : Subject<number> = new Subject();
   entityEdited$ = this.entityEditedSubject.asObservable();
 
+  data : any;
+
+  users$ = this.http.get("https://jsonplaceholder.typicode.com/users");
+  posts$ = this.http.get("https://jsonplaceholder.typicode.com/posts");
+
   notifyEdit(entityId: number){
     this.entityEditedSubject.next(entityId);
   }
 
-  constructor(private http : HttpClient){
+  constructor(private http : HttpClient, private mockDataService : MockDataService){
 
   }
 
   ngOnInit(): void {
 
-    this.callMultipleData();
+    // this.sequentialStreams();
+    this.callAggregateIntoSingleArray();
+   // this.mergeStreams();
 
-    this.callContinuousData();
+    // this.callMultipleData();
 
-    this.callUserAndCorrespondentPosts();
+    // this.callContinuousData();
+
+    // this.callUserAndCorrespondentPosts();
   }
 
   private callContinuousData(){
-    const users$ = this.http.get("https://jsonplaceholder.typicode.com/users");
     const timerAlwaysEmmiting$ = interval(3000).pipe(take(10));
 
     combineLatest({
-      users: users$,
+      users: this.users$,
       timerAlwaysEmmiting: timerAlwaysEmmiting$
-    }).subscribe({
-      next: (data : any)=>{
-        console.log(data);
-      }
-    })
+    }).subscribe(this.loggingObserver("call when all streams emited at least once."))
   }
 
   private callMultipleData(){
-    const users$ = this.http.get("https://jsonplaceholder.typicode.com/users");
-    const posts$ = this.http.get("https://jsonplaceholder.typicode.com/posts");
 
-    forkJoin({
-      users: users$,
-      posts: posts$
-    }).subscribe({
-      next: (data : any)=>{
-        console.log(data);
-      }
-    })
+
+    forkJoin({//? calls all streams at the same time. only when all complete, then the subscribe receives information.
+      users: this.users$,
+      posts: this.posts$
+    }).subscribe(this.loggingObserver("call when all streams complete"))
   }
 
   private callUserAndCorrespondentPosts(){
-    const users$ = this.http.get("https://jsonplaceholder.typicode.com/users");
 
-    users$.pipe(switchMap((users : any)=>{
+    this.users$.pipe(switchMap((users : any)=>{ //? converts one observable to another. in this case switch map to forkJoin.
       return forkJoin(this.converUsersToPostsObservables(users))
     }
-    )).subscribe({
-      next: (data : any)=>{
-        console.log(data);
-      }
+    )).subscribe(this.loggingObserver("callUserAndCorrespondentPosts"))
+  }
+
+  private callAggregateIntoSingleArray(){
+    of(1, 2, 3, 4).
+    pipe(
+      mergeMap((id : number)=>{ //calls all posts in parallel, without any order and merges into a single observable that will emit once by each  merged sub-observable.
+      return this.mockDataService.getPostsByUserId$(id);
+    }),
+    toArray(), //puts each post array into a single array of arrays
+    map((masterArray : any[])=>{ // transforms the array of arrays into a flattened single array.
+      return masterArray.flat();
     })
+    ).subscribe(this.loggingObserver("callAggregateIntoSingleArray"))
+  }
+
+  private sequentialStreams(){
+    concat(this.users$, this.posts$).subscribe(this.loggingObserver("one stream after another."))
+  }
+
+  private mergeStreams(){
+    merge(this.users$, this.posts$).subscribe(this.loggingObserver("merge:"))
   }
 
   private converUsersToPostsObservables(users : any){
-    return users.map((user: any)=> this.http.get(`https://jsonplaceholder.typicode.com/posts?userId=${user.id}`))
+    return users.map((user: any)=> this.mockDataService.getPostsByUserId$((user.id)))
+  }
+
+  private loggingObserver(name: string){
+    return {
+      next: (data : any)=>{
+        console.log(`[${name}]:`, data);
+        this.data = data;
+      },
+      error: (error : Error)=>{
+        //  Se umas das N requisições der erro, est
+        console.error(`[${name}]:`, error);
+      },
+      complete: ()=>{
+        //  Se umas das N requisições der erro, est
+        console.log(`[${name}]: observable stream completed`);
+      }
+
+    }
   }
 }
